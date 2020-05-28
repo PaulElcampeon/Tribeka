1f8ab36f3b301515e75510893b1ba87cb670a33f
PaulElcampeon <pauloladele@live.co.uk>
2020-05-29 00:11:03 +0100

    Setting Up scripts for project.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public GameObject patrolRef;
+
+    public int noOfPatrols;
+
+    private float waitTime;
+
+    private Vector3[] patrolPoints = new Vector3[4];
+
+    private bool isLeftPosAssigned, isRightPosAssigned, isUpPosAssigned, isDownPosAssigned;
+
+    private Vector3 leftStartPos, rightStartPos, upStartPos, downStartPos;
+
+    private SpriteRenderer spriteRender;
+
+    public int currentScale;
+    private int minScale = 2;
+    private int maxScale = 8;
+
+    /*scales and number of patrols
+     scale 2: 2 patrol
+     scale 3: up to 2
+     scale 4 up to 4*/
+
+    //max Scale is 4 x 4;
+    private void Awake()
+    {
+        spriteRender = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+            
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void AssignPatrolPoints()
+    {
+
+    }
+
+    private void AssignStartPositions()
+    {
+
+    }
+
+    private void AssignScale()
+    {
+        currentScale = Random.Range(minScale, maxScale);
+        transform.localScale = new Vector3(currentScale, currentScale, 0f);
+    }
+
+    private void AssignNoOfPatrols()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    private LineRenderer lazer;
+
+    private void Awake()
+    {
+        lazer = GetComponent<LineRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lazer.SetPosition(0, Vector3.up * 3);
+    }
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    private void Update()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up);
+        Debug.DrawLine(transform.position, hit.point);
+        lazer.SetPosition(0, transform.position);
+        lazer.SetPosition(1, hit.point);
+    }
+}
