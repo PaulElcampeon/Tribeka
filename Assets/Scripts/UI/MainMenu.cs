41ce75525e5e7a2485502c30cd6588eb7f2083a3
PaulElcampeon <pauloladele@live.co.uk>
2020-05-30 23:37:32 +0100

    Added SoundSlider script.

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -4,15 +4,79 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject settingsPanel;
+
+    [SerializeField]
+    private GameObject soundPanel;
+
+    [SerializeField]
+    private GameObject difficultyPanel;
+
+    [SerializeField]
+    private GameObject controlsPanel;
+
+    [SerializeField]
+    private GameObject creditsPanel;
+
+    public void OpenSettingsPanel()
     {
-        
+        SoundManager.instance.PlaySFX(1);
+
+        settingsPanel.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OpenSoundPanel()
     {
-        
+        SoundManager.instance.PlaySFX(1);
+
+        soundPanel.SetActive(true);
+    }
+
+    public void OpenControlsPanel()
+    {
+        SoundManager.instance.PlaySFX(1);
+
+        controlsPanel.SetActive(true);
+    }
+
+    public void OpenCreditsPanel()
+    {
+        SoundManager.instance.PlaySFX(1);
+
+        creditsPanel.SetActive(true);
+    }
+
+    public void OpenDifficultyPanel()
+    {
+        SoundManager.instance.PlaySFX(1);
+
+        difficultyPanel.SetActive(true);
+    }
+
+    public void SetDifficulty(int level)
+    {
+        SoundManager.instance.PlaySFX(1);
+
+        GameManager.instance.SetDifficulty(level);
+
+        difficultyPanel.SetActive(false);
+    }
+
+    public void CloseActivePanel()
+    {
+        SoundManager.instance.PlaySFX(2);
+
+        if (soundPanel.activeInHierarchy) { soundPanel.SetActive(false); return; }
+        if (controlsPanel.activeInHierarchy) { controlsPanel.SetActive(false); return; }
+        if (difficultyPanel.activeInHierarchy) { difficultyPanel.SetActive(false); return; }
+        if (settingsPanel.activeInHierarchy) { settingsPanel.SetActive(false); return; }
+        if (creditsPanel.activeInHierarchy) { creditsPanel.SetActive(false); return; }
+    }
+
+    public void LoadGame()
+    {
+        GameManager.instance.UnPause();
+        GameManager.instance.LoadScene("Game");
     }
 }
---
+++ Assets/Scripts/UI/SoundSliders.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SoundSliders : MonoBehaviour
+{
+    [SerializeField]
+    private Slider bgmSlider;
+
+    [SerializeField]
+    private Slider sfxSlider;
+
+    private void OnEnable()
+    {
+        bgmSlider.value = SoundManager.instance.bgmVolume;
+
+        sfxSlider.value = SoundManager.instance.sfxVolume;
+    }
+
+    public void UpdateBGMVolume()
+    {
+        SoundManager.instance.UpdatetBGMVolume(bgmSlider.value);
+    }
+
+    public void UpdateSFXVolume()
+    {
+        SoundManager.instance.UpdatetSFXVolume(sfxSlider.value);
+    }
+}
