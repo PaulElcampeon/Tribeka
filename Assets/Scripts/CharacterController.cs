c2a59f2182d04c1fd2c63763b354aa06b33fd725
PaulElcampeon <pauloladele@live.co.uk>
2020-05-30 19:51:34 +0100

    Modified script so that player can rotate his shield.

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -9,8 +9,14 @@ public class CharacterController : MonoBehaviour
 
     private Rigidbody2D rgb;
 
+    [SerializeField]
+    private GameObject shield;
+
     private float horizontal;
     private float vertical;
+    private float currentAngle = 0f;
+
+    private bool isTurning;
 
     private void Awake()
     {
@@ -22,6 +28,8 @@ public class CharacterController : MonoBehaviour
         MoveCamera();
 
         ComputePlayerInpute();
+
+        if (isTurning) RotateShield();
     }
 
     private void FixedUpdate()
@@ -33,10 +41,39 @@ public class CharacterController : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+
+        if (isTurning) return;
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentAngle == -360)
+            {
+                currentAngle = 0f;
+                shield.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+
+            //currentAngle += 90;
+            currentAngle -= 90;
+
+            isTurning = true;
+        }
     }
 
     private void MoveCamera()
     {
         Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+    }
+
+    private void RotateShield()
+    {
+        shield.transform.rotation = Quaternion.Euler(0f, 0f, Mathf.MoveTowardsAngle(shield.transform.rotation.eulerAngles.z, currentAngle, 100f * Time.deltaTime));
+
+        Debug.Log(shield.transform.rotation.eulerAngles.z);
+
+        //Debug.Log(Mathf.Abs(currentAngle - shield.transform.rotation.eulerAngles.z));
+        if (Mathf.Abs(currentAngle + (360 - shield.transform.rotation.eulerAngles.z)) <= 1)
+        {
+            isTurning = false;
+        }
     }
 }
