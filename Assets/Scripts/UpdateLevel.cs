13f19d0282b5e4e9f1fe741d6c34d2dda297bd94
PaulElcampeon <pauloladele@live.co.uk>
2020-06-01 19:24:59 +0100

    Added new script so that player can see the current level they are on.

---
+++ Assets/Scripts/UpdateLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UpdateLevel : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Text>().text = "Lv: " + GameManager.instance.difficulty;
+    }
+}
